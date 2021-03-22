15c4687a71bc5b5d0c3709f9c99e078383909b21
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2021-03-22 21:24:21 +0100

    fix: fix problem with validating cron

--- src/schema/dto/enqueue-repeating-schema.dto.ts
+++ src/schema/dto/enqueue-repeating-schema.dto.ts
@@ -1,6 +1,8 @@
+import { IsString } from 'class-validator';
 import { IsCron } from '../../common/decorator/validation/IsCron';
 
 export class EnqueueRepeatingSchemaDto {
+  @IsString()
   @IsCron()
   readonly cron: string;
 }
